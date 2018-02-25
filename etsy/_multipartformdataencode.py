d4a1f866aa85758e8f1c8ae3fd8b703e6c2ac07e
Forest Godfrey <fgodfrey@bigw.org>
2018-02-24 19:47:04 -0600

    Change multipart encoding to use array of bytes instead of strings/unicode. Fixes bug when uploading an image file where the UTF-8 encoder gets confused trying to encode the contents of the file (such as a JPEG) as if it were an ASCII string.

--- etsy/_multipartformdataencode.py
+++ etsy/_multipartformdataencode.py
@@ -6,6 +6,21 @@ Functions for encoding multipart/form-data
 From http://code.activestate.com/recipes/146306/ (PSF License)
 """
 
+class byteEncoder:
+    def __init__(self, sep):
+        self.b = None
+        self.sep = bytes(sep)
+
+    def append(self, val):
+        if self.b is None:
+            self.b = bytes(val)
+            return
+
+        self.b = self.b + self.sep + bytes(val)
+
+    def value(self):
+        return self.b
+
 
 def encode_multipart_formdata(fields, files):
     """
@@ -16,7 +31,7 @@ def encode_multipart_formdata(fields, files):
     """
     BOUNDARY = '----------ThIs_Is_tHe_bouNdaRY_$'
     CRLF = '\r\n'
-    L = []
+    L = byteEncoder(CRLF)
     for (key, value) in fields:
         L.append('--' + BOUNDARY)
         L.append('Content-Disposition: form-data; name="%s"' % key)
@@ -32,7 +47,7 @@ def encode_multipart_formdata(fields, files):
         L.append(value)
     L.append('--' + BOUNDARY + '--')
     L.append('')
-    body = CRLF.join(L)
+    body = L.value()
     content_type = 'multipart/form-data; boundary=%s' % BOUNDARY
     return content_type, body
 
